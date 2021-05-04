b693ea6874a78058b5c82d51988e953c65d5ef21
Maxel <gigaabait@yandex.ru>
2021-05-04 19:25:53 +0400

    grad

--- src/components/perfomanceBlock/perfomanceBlock.tsx
+++ src/components/perfomanceBlock/perfomanceBlock.tsx
@@ -15,13 +15,18 @@ const PerfomanceBlock = ({ blockData } : PerfomanceBlockProps) => {
         if(score >= 4 ) return 'rgba(123, 206, 60, 0.72)'
         if(score >= 3) return '#F1E469'
         return 'rgba(230, 62, 26, 0.72)'
+    }
 
+    const getDiagramBackground = (score: number) => {
+        if (score === 5) return {background: 'rgba(123, 206, 60, 0.72)'}
+        return {background: `linear-gradient(180deg, #FFFFFF ${getDiagramPercentage(score)}%, ${getDiagramColors(score)} 100%)`}
     }
 
     const {name, average_score} = blockData;
+
     return (
         <div className="perfomance-block">
-           <div className="perfomance-block__diagram" style={{background: `linear-gradient(180deg, #FFFFFF ${getDiagramPercentage(average_score)}%, ${getDiagramColors(average_score)} 100%)`}} />
+           <div className="perfomance-block__diagram" style={getDiagramBackground(average_score)} />
            <div className="perfomance-block__info">
                 <p className="perfomance-block__name">{name}</p>
                 <p className="perfomance-block__score">{`Средний балл - ${average_score}`}</p>
